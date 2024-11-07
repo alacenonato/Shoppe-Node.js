35e6922f8bf98236765d4e770a66d1cadf200bf2
Alace Pereira Nonato <alac_pereira@hotmail.com>
2024-11-07 14:41:05 -0300

    Assinatura Cart.js

---
+++ src/services/cart.js
@@ -0,0 +1,21 @@
+// quais ações meu carrinho pode fazer
+
+// casos de uso
+// ->adiciona um item no carrinho
+async function addItem(userCart) {
+
+}
+// ->deletar um item do carrinho
+async function deleteItem(userCart, name) {
+
+}
+// ->remover um item do carrinho 
+async function removeItem(userCart, index) {
+
+}
+
+// ->calcular o total da compra
+async function calculateTotal(userCart) {
+    
+}
+
