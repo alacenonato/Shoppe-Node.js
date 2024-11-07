9c48e4c116dcba81498d7d778eaf0617b2fb334d
Alace Pereira Nonato <alac_pereira@hotmail.com>
2024-11-07 17:33:51 -0300

    Delete Item

--- src/index.js
+++ src/index.js
@@ -9,8 +9,13 @@ console.log("Welcome to the your Shopee Cart!")
 const item1 = await createItem("hotwheels ferrari", 20.99, 1);
 const item2 = await createItem("hotwheels lamborghini", 39.99, 3);
 
+// adicione dois item ao carrinho
 await cartService.addItem(myCart, item1);
-await cartService.addItem(myWishList, item2);
+await cartService.addItem(myCart, item2);
+
+// deletei dois itens no carrinho
+await cartService.deleteItem(myCart, item2.name);
+await cartService.deleteItem(myCart, item1.name);
 
 console.log("Shopee Cart TOTAL IS:")
 await cartService.calculateTotal(myCart);
--- src/services/cart.js
+++ src/services/cart.js
@@ -7,6 +7,10 @@ async function addItem(userCart, item) {
 }
 // ->deletar um item do carrinho
 async function deleteItem(userCart, name) {
+    const index = userCart.findIndex((item) =>item.name === name);
+    if (index !== -1) {
+        userCart.splice(index, 1);
+    }
 
 }
 // ->remover um item do carrinho 
